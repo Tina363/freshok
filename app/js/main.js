608f1d0de54c43a57428bd6a6a2eb2cb058789dd
Tina363 <titanic008@rambler.ru>
2021-09-30 12:54:12 +0300

    Сделала правки

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,18 @@
 $(function(){
-  $('.header__slider').slick({
-    prevArrow: '<button type="button" class="slick-prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"></button>',
+  $('.slider__wrapper').slick({
+    prevArrow: '<button type="button" class="slider__arrow-left"></button>',
+    nextArrow: '<button type="button" class="slider__arrow-right"></button>',
     fade: true, 
     // autoplay: true,
     // autoplaySpeed: 5000
   });
+
+  $(".star").rateYo({
+    starWidth: "16px",
+    normalFill: "#A0A0A0",
+    ratedFill: "#E74C3C",
+    readOnly: true
+  });
+
+  var mixer = mixitup ('.top-products__inner');
 });
